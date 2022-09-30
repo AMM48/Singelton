cbee731baa4514885f4290d48eb4c3638794dc7e
AMM48 <83496936+AMM48@users.noreply.github.com>
2022-09-30 07:42:54 +0300

    Add files via upload

--- App.java
+++ App.java
@@ -1,3 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.singelton;
+
+/**
+ *
+ * @author Abdulrahman-PC
+ */
+
 public class App{
   public static void main(String[]args){
     Order o1 = new Order();
--- Logger.java
+++ Logger.java
@@ -1,4 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.singelton;
 
+/**
+ *
+ * @author Abdulrahman-PC
+ */
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
